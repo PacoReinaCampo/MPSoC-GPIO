230d43e6b67dc91dd4e2246b07f60fc3232ae6ee
PacoReinaCampo <frareicam@gmail.com>
2021-02-15 20:47:21 +0100

    Working in Peripheral-GPIO

--- sim/mixed/tests/core/ahb3/msim/simulate.bat
+++ sim/mixed/tests/core/ahb3/msim/simulate.bat
@@ -1,4 +1,8 @@
+@echo off
+source ../../../../../../settings64_msim.sh
+
 vlib work
-vlog -sv +incdir+../../../../../../rtl/verilog/ahb3/pkg -f system.verilog.vc
+vlog -sv -f system.verilog.vc
 vcom -2008 -f system.vhdl.vc
 vsim -c -do run.do work.mpsoc_gpio_testbench
+pause
--- sim/verilog/tests/core/wb/verilator/simulate.bat
+++ sim/verilog/tests/core/wb/verilator/simulate.bat
@@ -1,5 +1,5 @@
 @echo off
 call ../../../../../../settings64_verilator.bat
 
-verilator -Wno-lint -Wno-UNOPTFLAT -Wno-COMBDLY +incdir+../../../../../../rtl/verilog/ahb3/pkg --cc -f system.vc --top-module mpsoc_gpio_testbench
+verilator -Wno-lint -Wno-UNOPTFLAT -Wno-COMBDLY --cc -f system.vc --top-module mpsoc_gpio_testbench
 pause
