b4d68efd851bf868aa8495cfdb9be731cc408dc6
PacoReinaCampo <frareicam@gmail.com>
2023-02-22 22:07:06 +0100

    Working in Peripheral-GPIO 🔑

--- sim/vhdl/tests/peripheral/ahb3/ghdl/simulate.bat
+++ sim/vhdl/tests/peripheral/ahb3/ghdl/simulate.bat
@@ -1,11 +1,11 @@
 @echo off
 call ../../../../../../settings64_ghdl.bat
 
-ghdl -a --std=08 ../../../../../../rtl/vhdl/ahb3/pkg/vhdl_pkg.vhd
-ghdl -a --std=08 ../../../../../../rtl/vhdl/ahb3/pkg/peripheral_ahb3_pkg.vhd
-ghdl -a --std=08 ../../../../../../rtl/vhdl/ahb3/core/peripheral_apb42ahb3.vhd
-ghdl -a --std=08 ../../../../../../rtl/vhdl/ahb3/core/peripheral_gpio_apb4.vhd
-ghdl -a --std=08 ../../../../../../bench/vhdl/tests/core/ahb3/peripheral_gpio_testbench.vhd
+ghdl -a --std=08 ../../../../../../rtl/vhdl/pkg/core/vhdl_pkg.vhd
+ghdl -a --std=08 ../../../../../../rtl/vhdl/pkg/peripheral/ahb3/peripheral_ahb3_pkg.vhd
+ghdl -a --std=08 ../../../../../../rtl/vhdl/peripheral/ahb3/peripheral_apb42ahb3.vhd
+ghdl -a --std=08 ../../../../../../rtl/vhdl/peripheral/ahb3/peripheral_gpio_apb4.vhd
+ghdl -a --std=08 ../../../../../../bench/vhdl/tests/peripheral/ahb3/peripheral_gpio_testbench.vhd
 ghdl -m --std=08 peripheral_gpio_testbench
 ghdl -r --std=08 peripheral_gpio_testbench --ieee-asserts=disable-at-0 --disp-tree=inst > peripheral_gpio_testbench.tree
 pause
