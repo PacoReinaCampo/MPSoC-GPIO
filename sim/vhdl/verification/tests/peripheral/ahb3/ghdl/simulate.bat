4f233069d75531bfef50680904804ec64e883379
PacoReinaCampo <frareicam@gmail.com>
2023-04-12 17:06:31 +0200

    Working in Peripheral-GPIO 🔑

--- sim/vhdl/verification/tests/peripheral/ahb3/ghdl/simulate.bat
+++ sim/vhdl/verification/tests/peripheral/ahb3/ghdl/simulate.bat
@@ -47,9 +47,12 @@ call ../../../../../../../settings64_ghdl.bat
 
 ghdl -a --std=08 ../../../../../../../rtl/vhdl/code/pkg/core/vhdl_pkg.vhd
 ghdl -a --std=08 ../../../../../../../rtl/vhdl/code/pkg/peripheral/ahb3/peripheral_ahb3_pkg.vhd
+
 ghdl -a --std=08 ../../../../../../../rtl/vhdl/code/peripheral/ahb3/peripheral_apb42ahb3.vhd
 ghdl -a --std=08 ../../../../../../../rtl/vhdl/code/peripheral/ahb3/peripheral_gpio_apb4.vhd
+
 ghdl -a --std=08 ../../../../../../../bench/vhdl/code/tests/peripheral/ahb3/peripheral_gpio_testbench.vhd
+	
 ghdl -m --std=08 peripheral_gpio_testbench
 ghdl -r --std=08 peripheral_gpio_testbench --ieee-asserts=disable-at-0 --disp-tree=inst > peripheral_gpio_testbench.tree
 pause
--- sim/vhdl/verification/tests/peripheral/bb/ghdl/simulate.bat
+++ sim/vhdl/verification/tests/peripheral/bb/ghdl/simulate.bat
@@ -46,7 +46,9 @@
 call ../../../../../../../settings64_ghdl.bat
 
 ghdl -a --std=08 ../../../../../../../rtl/vhdl/code/peripheral/wb/peripheral_gpio_wb.vhd
+
 ghdl -a --std=08 ../../../../../../../bench/vhdl/code/tests/peripheral/wb/peripheral_gpio_testbench.vhd
+	
 ghdl -m --std=08 peripheral_gpio_testbench
 ghdl -r --std=08 peripheral_gpio_testbench --ieee-asserts=disable-at-0 --disp-tree=inst > peripheral_gpio_testbench.tree
 pause
