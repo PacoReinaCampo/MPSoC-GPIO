100b95c537d7025b63958d6094eb9b3d3b9677ce
PacoReinaCampo <frareicam@gmail.com>
2023-09-24 00:07:59 +0400

    Working in Peripheral-GPIO 🔑

--- sim/vhdl/verification/tests/peripheral/bb/ghdl/simulate.bat
+++ sim/vhdl/verification/tests/peripheral/bb/ghdl/simulate.bat
@@ -49,6 +49,6 @@ ghdl -a --std=08 ../../../../../../../rtl/vhdl/code/peripheral/wb/peripheral_gpi
 
 ghdl -a --std=08 ../../../../../../../bench/vhdl/code/tests/peripheral/wb/peripheral_gpio_testbench.vhd
 	
-ghdl -m --std=08 peripheral_gpio_testbench
-ghdl -r --std=08 peripheral_gpio_testbench --ieee-asserts=disable-at-0 --disp-tree=inst > peripheral_gpio_testbench.tree
+ghdl -e --std=08 peripheral_gpio_testbench
+ghdl -r --std=08 peripheral_gpio_testbench --ieee-asserts=disable-at-0 --vcd=peripheral_gpio_testbench.vcd --wave=system.ghw --stop-time=1ms
 pause
